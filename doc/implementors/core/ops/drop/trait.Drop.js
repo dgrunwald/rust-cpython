e95f81e024206fd1fa7ccb86642272626b6e998f
Mark Thomas <mbthomas@fb.com>
2018-02-27 11:18:07 -0800

    Update documentation

---
+++ doc/implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["cpython"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"cpython/struct.PyObject.html\" title=\"struct cpython::PyObject\">PyObject</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"cpython/struct.GILGuard.html\" title=\"struct cpython::GILGuard\">GILGuard</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"cpython/buffer/struct.PyBuffer.html\" title=\"struct cpython::buffer::PyBuffer\">PyBuffer</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
