51dea15cac60f9bcf81f7554eefd1afd29dee2eb
Mark Thomas <mbthomas@fb.com>
2018-02-27 10:21:35 -0800

    Ignore python code in rustdoc block

--- src/objectprotocol.rs
+++ src/objectprotocol.rs
@@ -74,7 +74,7 @@ pub trait ObjectProtocol : PythonObject {
     /// On Python 2, this is equivalent to the Python expression 'cmp(self, other)'.
     ///
     /// On Python 3, this is equivalent to:
-    /// ```
+    /// ```ignore
     /// if self == other:
     ///     return Equal
     /// elif a < b:
