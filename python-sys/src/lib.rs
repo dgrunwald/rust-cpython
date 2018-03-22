6a9b66d4acaed6c9878325b9b258c3c2ee02b981
Sam Vevang <sam.vevang@gmail.com>
2018-03-22 16:25:11 -0500

    vendor new python-sys in workspace

---
+++ python-sys/src/lib.rs
@@ -0,0 +1,13 @@
+use std::path::{Path, PathBuf};
+
+include!(concat!(env!("OUT_DIR"), "/python_path_funcs.rs"));
+
+pub fn python_path() -> Option<PathBuf> {
+    match python_build_prefix() {
+        Some(pth) => Some(Path::new(pth).join(Path::new("bin/python"))),
+        None => None
+    }
+}
+
+
+
