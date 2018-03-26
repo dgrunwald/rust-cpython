7b686015ec2b46a86c8c4a7eac8e01d3da2d22ed
Sam Vevang <sam.vevang@gmail.com>
2018-03-26 11:02:18 -0500

    some simple pip python helpers

--- python-sys/src/lib.rs
+++ python-sys/src/lib.rs
@@ -1,4 +1,6 @@
 use std::path::{Path, PathBuf};
+use std::process::{Command, Stdio, ExitStatus};
+use std::io::{Error, ErrorKind};
 
 include!(concat!(env!("OUT_DIR"), "/python_path_funcs.rs"));
 
@@ -9,5 +11,34 @@ pub fn python_path() -> Option<PathBuf> {
     }
 }
 
+pub fn pip_path() -> Option<PathBuf> {
+    match python_build_prefix() {
+        Some(pth) => Some(Path::new(pth).join(Path::new("bin/pip"))),
+        None => None
+    }
+}
 
+pub fn install_python_package(code_path: PathBuf, requirements_path: Option<PathBuf>) -> Result<ExitStatus, std::io::Error> {
+    let pip_path = pip_path().unwrap();
+    let python_path = python_path().unwrap();
 
+    match requirements_path {
+        Some(req_path) => {
+            let _ = Command::new(&python_path)
+                .args(&[pip_path.to_str().unwrap(), "install", "-r", req_path.to_str().unwrap()])
+                .status();
+            ()
+
+        },
+        None => ()
+    };
+    match python_build_prefix() {
+        Some(_prefix) => {
+            Command::new(&python_path)
+                .args(&[code_path.join("setup.py").to_str().unwrap(), "install"])
+                .stdout(Stdio::inherit())
+                .status()
+        },
+        None => Err(Error::new(ErrorKind::Other, "There was a problem running python"))
+    }
+}
