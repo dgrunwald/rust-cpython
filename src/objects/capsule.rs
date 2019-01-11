2bd33d47104750d2acd16c145549cd19ab6e958d
Georges Racinet <georges@racinet.fr>
2019-01-11 11:31:46 +0100

    Moved doc-comment from impl block to struct declaration

--- src/objects/capsule.rs
+++ src/objects/capsule.rs
@@ -8,7 +8,181 @@ use python::{Python, ToPythonPointer};
 use std::ffi::{CStr, CString, NulError};
 use std::mem;
 
-/// Represents a Python capsule object.
+/// Capsules are the preferred way to export/import C APIs between extension modules,
+/// see [Providing a C API for an Extension Module](https://docs.python.org/3/extending/extending.html#using-capsules).
+///
+/// In particular, capsules can be very useful to start adding Rust extensions besides
+/// existing traditional C ones, be it for gradual rewrites or to extend with new functionality.
+/// They can also be used for interaction between independently compiled Rust extensions if needed.
+///
+/// Capsules can point to data, usually static arrays of constants and function pointers,
+/// or to function pointers directly. These two cases have to be handled differently in Rust,
+/// and the latter is possible only for architectures were data and function pointers have
+/// the same sizes.
+///
+/// # Examples
+/// ## Using a capsule defined in another extension module
+/// This retrieves and use one of the simplest capsules in the Python standard library, found in
+/// the `unicodedata` module. The C API enclosed in this capsule is the same for all Python
+/// versions supported by this crate. This is not the case of all capsules from the standard
+/// library. For instance the `struct` referenced by `datetime.datetime_CAPI` gets a new member
+/// in version 3.7.
+///
+/// Note: this example is a lower-level version of the [`py_capsule!`] example. Only the
+/// capsule retrieval actually differs.
+/// ```
+/// #[macro_use] extern crate cpython;
+/// extern crate libc;
+///
+/// use cpython::{Python, PyCapsule};
+/// use libc::{c_char, c_int};
+/// use std::ffi::{c_void, CStr, CString};
+/// use std::mem;
+/// use std::ptr::null_mut;
+///
+/// #[allow(non_camel_case_types)]
+/// type Py_UCS4 = u32;
+/// const UNICODE_NAME_MAXLEN: usize = 256;
+///
+/// #[repr(C)]
+/// pub struct unicode_name_CAPI {
+///     // the `ucd` signature arguments are actually optional (can be `NULL`) FFI PyObject
+///     // pointers used to pass alternate (former) versions of Unicode data.
+///     // We won't need to use them with an actual value in these examples, so it's enough to
+///     // specify them as `*mut c_void`, and it spares us a direct reference to the lower
+///     // level Python FFI bindings.
+///     size: c_int,
+///     getname: unsafe extern "C" fn(
+///         ucd: *mut c_void,
+///         code: Py_UCS4,
+///         buffer: *mut c_char,
+///         buflen: c_int,
+///         with_alias_and_seq: c_int,
+///     ) -> c_int,
+///     getcode: unsafe extern "C" fn(
+///         ucd: *mut c_void,
+///         name: *const c_char,
+///         namelen: c_int,
+///         code: *mut Py_UCS4,
+///     ) -> c_int,
+/// }
+///
+/// #[derive(Debug, PartialEq)]
+/// pub enum UnicodeDataError {
+///     InvalidCode,
+///     UnknownName,
+/// }
+///
+/// impl unicode_name_CAPI {
+///     pub fn get_name(&self, code: Py_UCS4) -> Result<CString, UnicodeDataError> {
+///         let mut buf: Vec<c_char> = Vec::with_capacity(UNICODE_NAME_MAXLEN);
+///         let buf_ptr = buf.as_mut_ptr();
+///         if unsafe {
+///           ((*self).getname)(null_mut(), code, buf_ptr, UNICODE_NAME_MAXLEN as c_int, 0)
+///         } != 1 {
+///             return Err(UnicodeDataError::InvalidCode);
+///         }
+///         mem::forget(buf);
+///         Ok(unsafe { CString::from_raw(buf_ptr) })
+///     }
+///
+///     pub fn get_code(&self, name: &CStr) -> Result<Py_UCS4, UnicodeDataError> {
+///         let namelen = name.to_bytes().len() as c_int;
+///         let mut code: [Py_UCS4; 1] = [0; 1];
+///         if unsafe {
+///             ((*self).getcode)(null_mut(), name.as_ptr(), namelen, code.as_mut_ptr())
+///         } != 1 {
+///             return Err(UnicodeDataError::UnknownName);
+///         }
+///         Ok(code[0])
+///     }
+/// }
+///
+/// let gil = Python::acquire_gil();
+/// let py = gil.python();
+///
+/// let capi: &unicode_name_CAPI = unsafe {
+///     PyCapsule::import_data(
+///         py,
+///         CStr::from_bytes_with_nul_unchecked(b"unicodedata.ucnhash_CAPI\0"),
+///     )
+/// }
+/// .unwrap();
+///
+/// assert_eq!(capi.get_name(32).unwrap().to_str(), Ok("SPACE"));
+/// assert_eq!(capi.get_name(0), Err(UnicodeDataError::InvalidCode));
+///
+/// assert_eq!(
+///     capi.get_code(CStr::from_bytes_with_nul(b"COMMA\0").unwrap()),
+///     Ok(44)
+/// );
+/// assert_eq!(
+///     capi.get_code(CStr::from_bytes_with_nul(b"\0").unwrap()),
+///     Err(UnicodeDataError::UnknownName)
+/// );
+/// ```
+///
+/// ## Creating a capsule from Rust
+/// In this example, we enclose some data and a function in a capsule, using an intermediate
+/// `struct` as enclosing type, then retrieve them back and use them.
+///
+/// ```
+/// extern crate cpython;
+/// extern crate libc;
+///
+/// use libc::c_int;
+/// use cpython::{PyCapsule, Python};
+/// use std::ffi::{c_void, CStr, CString};
+///
+/// #[repr(C)]
+/// struct CapsData {
+///     value: c_int,
+///     fun: fn(c_int, c_int) -> c_int,
+/// }
+///
+/// fn add(a: c_int, b: c_int) -> c_int {
+///     a + b
+/// }
+///
+/// const DATA: CapsData = CapsData{value: 1, fun: add};
+///
+/// let gil = Python::acquire_gil();
+/// let py = gil.python();
+/// let caps = PyCapsule::new_data(py, &mut DATA, "somemod.capsdata").unwrap();
+///
+/// let retrieved: &CapsData = unsafe {caps.data_ref("somemod.capsdata")}.unwrap();
+/// assert_eq!(retrieved.value, 1);
+/// assert_eq!((retrieved.fun)(2 as c_int, 3 as c_int), 5);
+/// ```
+///
+/// Of course, a more realistic example would be to store the capsule in a Python module,
+/// allowing another extension (possibly foreign) to retrieve and use it.
+/// Note that in that case, the capsule `name` must be full dotted name of the capsule object,
+/// as we're doing here.
+/// ```
+/// # #[macro_use] extern crate cpython;
+/// # extern crate libc;
+/// # use libc::c_int;
+/// # use cpython::PyCapsule;
+/// # #[repr(C)]
+/// # struct CapsData {
+/// #     value: c_int,
+/// #     fun: fn(c_int, c_int) -> c_int,
+/// # }
+/// # fn add(a: c_int, b: c_int) -> c_int {
+/// #     a + b
+/// # }
+/// # const DATA: CapsData = CapsData{value: 1, fun: add};
+/// py_module_initializer!(somemod, initsomemod, PyInit_somemod, |py, m| {
+///   m.add(py, "__doc__", "A module holding a capsule")?;
+///   m.add(py, "capsdata", PyCapsule::new_data(py, &mut DATA, "somemod.capsdata").unwrap())?;
+///   Ok(())
+/// });
+/// ```
+/// Another Rust extension could then declare `CapsData` and use `PyCapsule::import_data` to
+/// fetch it back.
+///
+/// [`py_capsule!`]: macro.py_capsule.html
 pub struct PyCapsule(PyObject);
 
 pyobject_newtype!(PyCapsule, PyCapsule_CheckExact, PyCapsule_Type);
@@ -267,181 +441,6 @@ macro_rules! py_capsule_fn {
     )
 }
 
-/// Capsules are the preferred way to export/import C APIs between extension modules,
-/// see [Providing a C API for an Extension Module](https://docs.python.org/3/extending/extending.html#using-capsules).
-///
-/// In particular, capsules can be very useful to start adding Rust extensions besides
-/// existing traditional C ones, be it for gradual rewrites or to extend with new functionality.
-/// They can also be used for interaction between independently compiled Rust extensions if needed.
-///
-/// Capsules can point to data, usually static arrays of constants and function pointers,
-/// or to function pointers directly. These two cases have to be handled differently in Rust,
-/// and the latter is possible only for architectures were data and function pointers have
-/// the same sizes.
-///
-/// # Examples
-/// ## Using a capsule defined in another extension module
-/// This retrieves and use one of the simplest capsules in the Python standard library, found in
-/// the `unicodedata` module. The C API enclosed in this capsule is the same for all Python
-/// versions supported by this crate. This is not the case of all capsules from the standard
-/// library. For instance the `struct` referenced by `datetime.datetime_CAPI` gets a new member
-/// in version 3.7.
-///
-/// Note: this example is a lower-level version of the [`py_capsule!`] example. Only the
-/// capsule retrieval actually differs.
-/// ```
-/// #[macro_use] extern crate cpython;
-/// extern crate libc;
-///
-/// use cpython::{Python, PyCapsule};
-/// use libc::{c_char, c_int};
-/// use std::ffi::{c_void, CStr, CString};
-/// use std::mem;
-/// use std::ptr::null_mut;
-///
-/// #[allow(non_camel_case_types)]
-/// type Py_UCS4 = u32;
-/// const UNICODE_NAME_MAXLEN: usize = 256;
-///
-/// #[repr(C)]
-/// pub struct unicode_name_CAPI {
-///     // the `ucd` signature arguments are actually optional (can be `NULL`) FFI PyObject
-///     // pointers used to pass alternate (former) versions of Unicode data.
-///     // We won't need to use them with an actual value in these examples, so it's enough to
-///     // specify them as `*mut c_void`, and it spares us a direct reference to the lower
-///     // level Python FFI bindings.
-///     size: c_int,
-///     getname: unsafe extern "C" fn(
-///         ucd: *mut c_void,
-///         code: Py_UCS4,
-///         buffer: *mut c_char,
-///         buflen: c_int,
-///         with_alias_and_seq: c_int,
-///     ) -> c_int,
-///     getcode: unsafe extern "C" fn(
-///         ucd: *mut c_void,
-///         name: *const c_char,
-///         namelen: c_int,
-///         code: *mut Py_UCS4,
-///     ) -> c_int,
-/// }
-///
-/// #[derive(Debug, PartialEq)]
-/// pub enum UnicodeDataError {
-///     InvalidCode,
-///     UnknownName,
-/// }
-///
-/// impl unicode_name_CAPI {
-///     pub fn get_name(&self, code: Py_UCS4) -> Result<CString, UnicodeDataError> {
-///         let mut buf: Vec<c_char> = Vec::with_capacity(UNICODE_NAME_MAXLEN);
-///         let buf_ptr = buf.as_mut_ptr();
-///         if unsafe {
-///           ((*self).getname)(null_mut(), code, buf_ptr, UNICODE_NAME_MAXLEN as c_int, 0)
-///         } != 1 {
-///             return Err(UnicodeDataError::InvalidCode);
-///         }
-///         mem::forget(buf);
-///         Ok(unsafe { CString::from_raw(buf_ptr) })
-///     }
-///
-///     pub fn get_code(&self, name: &CStr) -> Result<Py_UCS4, UnicodeDataError> {
-///         let namelen = name.to_bytes().len() as c_int;
-///         let mut code: [Py_UCS4; 1] = [0; 1];
-///         if unsafe {
-///             ((*self).getcode)(null_mut(), name.as_ptr(), namelen, code.as_mut_ptr())
-///         } != 1 {
-///             return Err(UnicodeDataError::UnknownName);
-///         }
-///         Ok(code[0])
-///     }
-/// }
-///
-/// let gil = Python::acquire_gil();
-/// let py = gil.python();
-///
-/// let capi: &unicode_name_CAPI = unsafe {
-///     PyCapsule::import_data(
-///         py,
-///         CStr::from_bytes_with_nul_unchecked(b"unicodedata.ucnhash_CAPI\0"),
-///     )
-/// }
-/// .unwrap();
-///
-/// assert_eq!(capi.get_name(32).unwrap().to_str(), Ok("SPACE"));
-/// assert_eq!(capi.get_name(0), Err(UnicodeDataError::InvalidCode));
-///
-/// assert_eq!(
-///     capi.get_code(CStr::from_bytes_with_nul(b"COMMA\0").unwrap()),
-///     Ok(44)
-/// );
-/// assert_eq!(
-///     capi.get_code(CStr::from_bytes_with_nul(b"\0").unwrap()),
-///     Err(UnicodeDataError::UnknownName)
-/// );
-/// ```
-///
-/// ## Creating a capsule from Rust
-/// In this example, we enclose some data and a function in a capsule, using an intermediate
-/// `struct` as enclosing type, then retrieve them back and use them.
-///
-/// ```
-/// extern crate cpython;
-/// extern crate libc;
-///
-/// use libc::c_int;
-/// use cpython::{PyCapsule, Python};
-/// use std::ffi::{c_void, CStr, CString};
-///
-/// #[repr(C)]
-/// struct CapsData {
-///     value: c_int,
-///     fun: fn(c_int, c_int) -> c_int,
-/// }
-///
-/// fn add(a: c_int, b: c_int) -> c_int {
-///     a + b
-/// }
-///
-/// const DATA: CapsData = CapsData{value: 1, fun: add};
-///
-/// let gil = Python::acquire_gil();
-/// let py = gil.python();
-/// let caps = PyCapsule::new_data(py, &mut DATA, "somemod.capsdata").unwrap();
-///
-/// let retrieved: &CapsData = unsafe {caps.data_ref("somemod.capsdata")}.unwrap();
-/// assert_eq!(retrieved.value, 1);
-/// assert_eq!((retrieved.fun)(2 as c_int, 3 as c_int), 5);
-/// ```
-///
-/// Of course, a more realistic example would be to store the capsule in a Python module,
-/// allowing another extension (possibly foreign) to retrieve and use it.
-/// Note that in that case, the capsule `name` must be full dotted name of the capsule object,
-/// as we're doing here.
-/// ```
-/// # #[macro_use] extern crate cpython;
-/// # extern crate libc;
-/// # use libc::c_int;
-/// # use cpython::PyCapsule;
-/// # #[repr(C)]
-/// # struct CapsData {
-/// #     value: c_int,
-/// #     fun: fn(c_int, c_int) -> c_int,
-/// # }
-/// # fn add(a: c_int, b: c_int) -> c_int {
-/// #     a + b
-/// # }
-/// # const DATA: CapsData = CapsData{value: 1, fun: add};
-/// py_module_initializer!(somemod, initsomemod, PyInit_somemod, |py, m| {
-///   m.add(py, "__doc__", "A module holding a capsule")?;
-///   m.add(py, "capsdata", PyCapsule::new_data(py, &mut DATA, "somemod.capsdata").unwrap())?;
-///   Ok(())
-/// });
-/// ```
-/// Another Rust extension could then declare `CapsData` and use `PyCapsule::import_data` to
-/// fetch it back.
-///
-/// [`py_capsule!`]: macro.py_capsule.html
 impl PyCapsule {
     /// Retrieve the contents of a capsule pointing to some data as a reference.
     ///
