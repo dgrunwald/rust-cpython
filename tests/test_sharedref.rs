b69080077a1f681d359b16a91b2ff77c669d86b9
Yuya Nishihara <yuya@tcha.org>
2019-11-03 16:08:07 +0900

    Add utility to share rust reference across Python objects
    This was originally developed for the Mercurial's Rust extension. It allows
    us to implement a Python iterator over a Rust iterator _relatively_ safely.
    
    The problem is that a Rust iterator typically has a reference to the
    collection behind, and thus cannot be a data member of a Python object.
    We get around this problem by casting &'a to &'static and add runtime
    validation instead. The basic idea is described in the following blog post.
    
    https://raphaelgomes.dev/blog/articles/2019-07-01-sharing-references-between-python-and-rust.html
    
    In order to make the reference-sharing business safe, we have the following
    structs:
    
     - PySharedRefCell defines a storage holding a value to be shared
     - PySharedRef is a lifetime-bound reference to the value above, which provides
       a safe interface on top of the PySharedRefCell and its owner PyObject
     - UnsafePyLeaked is the reference not bound to the real lifetime, and its
       validity is checked at runtime
     - PyLeakedRef/PyLeakedRefMut are borrowed references from UnsafePyLeaked
    
    In order to create PySharedRef in safe manner, py_class!() macro extension
    will be added by later patches:
    
        py_class!(class List |py|
            @shared data vec: Vec<i32>;
            // the storage type is PySharedRefCell<Vec<i32>>, but its accessor
            // returns PySharedRef<'a, Vec<i32>>. It makes sure that the right
            // owner PyObject and its data member are paired.
        });
    
    There are a couple of unsafe business in this module, and some public
    functions are unfortunately unsafe. Please refer to the inline comments
    why some of them can be safe and some can't.
    
    Thanks to Georges Racinet for the basic ideas and experiments,
              Raphaël Gomès for the core implementation,
              Mark Thomas for the generation-counter idea,
              and Mercurial developers for reviewing the original patches.

---
+++ tests/test_sharedref.rs
@@ -0,0 +1,161 @@
+#[macro_use]
+extern crate cpython;
+
+use cpython::{GILGuard, PySharedRef, PySharedRefCell, Python, PythonObject};
+
+py_class!(class Owner |py| {
+    data string_: PySharedRefCell<String>;
+});
+
+// TODO: will be replaced with `py_class` macro.
+impl Owner {
+    /// Returns a safe reference to the shared `string_`.
+    fn string<'a>(&'a self, py: Python<'a>) -> PySharedRef<'a, String> {
+        let owner = self.as_object();
+        let data = self.string_(py);
+        unsafe { PySharedRef::new(py, owner, data) }
+    }
+}
+
+fn prepare_env() -> (GILGuard, Owner) {
+    let gil = Python::acquire_gil();
+    let owner = {
+        let py = gil.python();
+        Owner::create_instance(py, PySharedRefCell::new("new".to_owned())).unwrap()
+    };
+    (gil, owner)
+}
+
+#[test]
+fn test_leaked_borrow() {
+    let (gil, owner) = prepare_env();
+    let py = gil.python();
+    let leaked = owner.string(py).leak_immutable();
+    let leaked_ref = unsafe { leaked.try_borrow(py) }.unwrap();
+    assert_eq!(*leaked_ref, "new");
+}
+
+#[test]
+fn test_leaked_borrow_mut() {
+    let (gil, owner) = prepare_env();
+    let py = gil.python();
+    let leaked = owner.string(py).leak_immutable();
+    let mut leaked_iter = unsafe { leaked.map(py, |s| s.chars()) };
+    let mut leaked_ref = unsafe { leaked_iter.try_borrow_mut(py) }.unwrap();
+    assert_eq!(leaked_ref.next(), Some('n'));
+    assert_eq!(leaked_ref.next(), Some('e'));
+    assert_eq!(leaked_ref.next(), Some('w'));
+    assert_eq!(leaked_ref.next(), None);
+}
+
+#[test]
+fn test_leaked_borrow_after_mut() {
+    let (gil, owner) = prepare_env();
+    let py = gil.python();
+    let leaked = owner.string(py).leak_immutable();
+    owner.string(py).borrow_mut().clear();
+    assert!(unsafe { leaked.try_borrow(py) }.is_err());
+}
+
+#[test]
+fn test_leaked_borrow_mut_after_mut() {
+    let (gil, owner) = prepare_env();
+    let py = gil.python();
+    let leaked = owner.string(py).leak_immutable();
+    let mut leaked_iter = unsafe { leaked.map(py, |s| s.chars()) };
+    owner.string(py).borrow_mut().clear();
+    assert!(unsafe { leaked_iter.try_borrow_mut(py) }.is_err());
+}
+
+#[test]
+#[should_panic(expected = "map() over invalidated leaked reference")]
+fn test_leaked_map_after_mut() {
+    let (gil, owner) = prepare_env();
+    let py = gil.python();
+    let leaked = owner.string(py).leak_immutable();
+    owner.string(py).borrow_mut().clear();
+    let _leaked_iter = unsafe { leaked.map(py, |s| s.chars()) };
+}
+
+#[test]
+fn test_try_borrow_mut_while_leaked_ref() {
+    let (gil, owner) = prepare_env();
+    let py = gil.python();
+    assert!(owner.string(py).try_borrow_mut().is_ok());
+    let leaked = owner.string(py).leak_immutable();
+    {
+        let _leaked_ref = unsafe { leaked.try_borrow(py) }.unwrap();
+        assert!(owner.string(py).try_borrow_mut().is_err());
+        {
+            let _leaked_ref2 = unsafe { leaked.try_borrow(py) }.unwrap();
+            assert!(owner.string(py).try_borrow_mut().is_err());
+        }
+        assert!(owner.string(py).try_borrow_mut().is_err());
+    }
+    assert!(owner.string(py).try_borrow_mut().is_ok());
+}
+
+#[test]
+fn test_try_borrow_mut_while_leaked_ref_mut() {
+    let (gil, owner) = prepare_env();
+    let py = gil.python();
+    assert!(owner.string(py).try_borrow_mut().is_ok());
+    let leaked = owner.string(py).leak_immutable();
+    let mut leaked_iter = unsafe { leaked.map(py, |s| s.chars()) };
+    {
+        let _leaked_ref = unsafe { leaked_iter.try_borrow_mut(py) }.unwrap();
+        assert!(owner.string(py).try_borrow_mut().is_err());
+    }
+    assert!(owner.string(py).try_borrow_mut().is_ok());
+}
+
+#[test]
+fn test_try_leak_while_borrow_mut() {
+    let (gil, owner) = prepare_env();
+    let py = gil.python();
+    let _mut_ref = owner.string(py).borrow_mut();
+    assert!(owner.string(py).try_leak_immutable().is_err());
+}
+
+#[test]
+#[should_panic(expected = "already mutably borrowed")]
+fn test_leak_while_borrow_mut() {
+    let (gil, owner) = prepare_env();
+    let py = gil.python();
+    let _mut_ref = owner.string(py).borrow_mut();
+    owner.string(py).leak_immutable();
+}
+
+#[test]
+fn test_try_borrow_mut_while_borrow() {
+    let (gil, owner) = prepare_env();
+    let py = gil.python();
+    let _ref = owner.string(py).borrow();
+    assert!(owner.string(py).try_borrow_mut().is_err());
+}
+
+#[test]
+#[should_panic(expected = "already borrowed")]
+fn test_borrow_mut_while_borrow() {
+    let (gil, owner) = prepare_env();
+    let py = gil.python();
+    let _ref = owner.string(py).borrow();
+    owner.string(py).borrow_mut();
+}
+
+#[test]
+fn test_try_borrow_while_borrow_mut() {
+    let (gil, owner) = prepare_env();
+    let py = gil.python();
+    let _mut_ref = owner.string(py).borrow_mut();
+    assert!(owner.string(py).try_borrow().is_err());
+}
+
+#[test]
+#[should_panic(expected = "already mutably borrowed")]
+fn test_borrow_while_borrow_mut() {
+    let (gil, owner) = prepare_env();
+    let py = gil.python();
+    let _mut_ref = owner.string(py).borrow_mut();
+    owner.string(py).borrow();
+}
