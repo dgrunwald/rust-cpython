96d2b4f522282620e0275378b0114746d3e0f7c1
Mark Thomas <markbt@efaref.net>
2020-04-24 15:49:03 +0100

    suppress unused_parens warnings in sys crates
    The python27-sys and python3-sys crates have parentheses that rustc deems unnecessary.  Suppress this warning.

--- python27-sys/src/lib.rs
+++ python27-sys/src/lib.rs
@@ -1,5 +1,5 @@
 #![no_std]
-#![allow(non_camel_case_types, non_upper_case_globals, non_snake_case)]
+#![allow(non_camel_case_types, non_upper_case_globals, non_snake_case, unused_parens)]
 
 pub use crate::boolobject::*;
 pub use crate::bufferobject::*;
